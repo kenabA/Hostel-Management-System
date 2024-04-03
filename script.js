e0235954c2f2c248df7d226fe5887f30b1c02de0
kenabey <kebab.bahadur@gmail.com>
2024-04-03 11:50:36 +0545

    Added Login & Signup

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var knowMoreBtn = document.getElementById("know-more");
+var getStartedBtn = document.getElementById("get-started");
+var contactBtn = document.getElementById("contact-us-btn");
 var sectionHeader = document.getElementById("hero");
+var sectionFacilities = document.getElementById("facilities");
+var sectionFooter = document.getElementById("footer");
 var reserveAppBtn = document.querySelector(".reserve-appointment-btn");
 var heroContent = document.querySelector(".hero_content--texts");
 
@@ -25,9 +29,35 @@ knowMoreBtn.addEventListener("click", () => {
   }, 800);
 });
 
+getStartedBtn.addEventListener("click", () => {
+  sectionFacilities.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+contactBtn.addEventListener("click", () => {
+  sectionFooter.scrollIntoView({
+    behavior: "smooth",
+  });
+  setTimeout(() => {
+    outlineTxt();
+  }, 800);
+});
+
 const outlineBtn = () => {
   reserveAppBtn.style.backgroundColor = "#D8315B";
   setTimeout(() => {
     reserveAppBtn.style.backgroundColor = "#ef9b00";
   }, 1000);
 };
+
+const outlineTxt = () => {
+  reserveAppBtn.style.color = "#D8315B";
+  setTimeout(() => {
+    reserveAppBtn.style.color = "#ef9b00";
+  }, 1000);
+};
+
+function openNewPage() {
+  window.open("signup.html", "_blank");
+}
