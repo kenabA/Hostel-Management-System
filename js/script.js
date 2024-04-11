fa1694dc8ff33119cf004e64e60e48afafe4ef06
kenabey <kebab.bahadur@gmail.com>
2024-04-11 22:20:22 +0545

    Adding in details into the admin dashboard

--- js/script.js
+++ js/script.js
@@ -96,9 +96,3 @@ const outlineTxt = () => {
 function openNewPage() {
   window.open("signup.html", "_blank");
 }
-
-const hamBurger = document.querySelector(".toggle-btn");
-
-hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
-});
