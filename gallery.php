fcaa30c71c0dab5527f1e97b0e126cd634c81c60
Kenab <kebab.bahadur@gmail.com>
2024-05-10 12:23:46 +0545

    adding in gallery

---
+++ gallery.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Hamrostel : Gallery</title>
+  <link rel="stylesheet" href="./assets/css/style.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+
+<body>
+  <!-----=====-----===== SECTION : HEADER =====-----=====----->
+  <header id="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary pb-24 pt-24">
+      <div class="container">
+        <a class="text-white navbar-brand fw-semibold" href="#section-hero">
+          <i class="fa-solid font-18 fa-house-user bg-primary-tint text-primary p-8 rounded-5 me-8"></i>
+          Hamrostel</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+          <div class="navbar-nav gap-32 align-items-center fw-medium fs-24">
+            <a class="nav-link text-white fw-semibold" aria-current="page" href="./index.php#hero">Home</a>
+            <a class="nav-link text-white fw-semibold" href="./index.php#facilities">Facilities</a>
+            <a class="nav-link text-white fw-semibold" href="./index.php#rooms">Rooms</a>
+            <a class="nav-link text-white fw-semibold" href="./index.php#events">Events</a>
+            <a class="nav-link text-white fw-semibold" href="./index.php#contactus">Contact Us</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <main>
+    <!-----=====-----===== SECTION : GALLERY =====-----=====----->
+    <section class="section gallery py-96">
+      <div class="container">
+        <div class="gallery_content--texts text-center mb-48">
+          <div class="gallery_content--header mb-24">
+            <span class="d-block text-primary font-14 fw-semibold">VIEW GALLERY</span>
+            <h2 class="text-beta">Our Gallery</h2>
+          </div>
+          <div class="gallery_content--para">
+            <p class="lh-para font-16 text-gray-600">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur laborum dolorum quaerat sint iusto eaque.
+            </p>
+          </div>
+        </div>
+        <div class="row align-items-center gy-48 gx-32">
+          <div class="col-4 col-lg-4 col-md-6 col-sm-12 gallery_imgWr">
+            <img style="
+                  height: 300px;
+                  width: 100%;
+                  object-fit: cover;
+                  object-position: center;
+                " src="./hostel_img/hostel-1.jpeg" alt="Responsive image" />
+          </div>
+          <div class="col-4 col-lg-4 col-md-6 col-sm-12 gallery_imgWr">
+            <img style="
+                  height: 300px;
+                  width: 100%;
+                  object-fit: cover;
+                  object-position: center;
+                " src="./hostel_img/hostel-2.jpg" alt="Responsive image" />
+          </div>
+          <div class="col-4 col-lg-4 col-md-6 col-sm-12 gallery_imgWr">
+            <img style="
+                  height: 300px;
+                  width: 100%;
+                  object-fit: cover;
+                  object-position: center;
+                " src="./hostel_img/hostel-3.jpeg" alt="Responsive image" />
+          </div>
+          <div class="col-4 col-lg-4 col-md-6 col-sm-12 gallery_imgWr">
+            <img style="
+                  height: 300px;
+                  width: 100%;
+                  object-fit: cover;
+                  object-position: center;
+                " src="./hostel_img/hostel-4.jpeg" alt="Responsive image" />
+          </div>
+          <div class="col-4 col-lg-4 col-md-6 col-sm-12 gallery_imgWr">
+            <img style="
+                  height: 300px;
+                  width: 100%;
+                  object-fit: cover;
+                  object-position: center;
+                " src="./hostel_img/hostel-5.jpeg" alt="Responsive image" />
+          </div>
+          <div class="col-4 col-lg-4 col-md-6 col-sm-12 gallery_imgWr">
+            <img style="
+                  height: 300px;
+                  width: 100%;
+                  object-fit: cover;
+                  object-position: center;
+                " src="./hostel_img/hostel-6.jpeg" alt="Responsive image" />
+          </div>
+          <div class="col-4 col-lg-4 col-md-6 col-sm-12 gallery_imgWr">
+            <img style="
+                  height: 300px;
+                  width: 100%;
+                  object-fit: cover;
+                  object-position: center;
+                " src="./hostel_img/hostel-7.jpeg" alt="Responsive image" />
+          </div>
+          <div class="col-4 col-lg-4 col-md-6 col-sm-12 gallery_imgWr">
+            <img style="
+                  height: 300px;
+                  width: 100%;
+                  object-fit: cover;
+                  object-position: center;
+                " src="./hostel_img/hostel-8.jpeg" alt="Responsive image" />
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <!-- FOOTER -->
+  <footer id="footer" class="section footer py-96">
+    <div class="container">
+      <div class="row gy-48 gx-32">
+        <div class="col-md-4 footer_content--texts order-0 col-sm-12">
+          <div class="footer_content--texts-header mb-32">
+            <a class="font-24 text-primary navbar-brand fw-semibold mb-18" href="#header"><i
+                class="fa-solid fa-house-user"></i> Hamrostel</a>
+            <p class="font-16 text-black mt-12">
+              An experience, you won't regret.
+            </p>
+          </div>
+          <div class="footer_content--texts-mediasBox mb-48">
+            <p class="fw-medium text-gray-600">Social Medias:</p>
+            <div class="footer_content--texts-medias d-flex gap-18">
+              <a class="text-primary-shade--1" href="#"><i class="font-24 fa-brands fa-facebook"></i></a>
+              <a class="text-primary-shade--1" href="#"><i class="font-24 fa-brands fa-instagram"></i></a>
+              <a class="text-primary-shade--1" href="#"><i class="font-24 fa-brands fa-twitter"></i></a>
+            </div>
+          </div>
+          <p class="font-12 fw-normal text-gray-500">
+            Copyright © <span class="year">2024</span> by Omnifood, Inc. All
+            rights reserved.
+          </p>
+        </div>
+        <div class="col-md-4 footer_content--texts order-1 col-sm-6">
+          <div class="footer_content--texts-header mb-24">
+            <h4 class="font-18 fw-semibold text-black">Quick Links</h4>
+          </div>
+          <ul class="d-flex flex-column gap-18 p-0">
+            <li class="ls-none">
+              <a class="td-none footer_content--texts-link font-16 fw-medium text-gray-600"
+                href="./index.php#hero">Home</a>
+            </li>
+            <li class="font-16 ls-none fw-medium text-gray-600">
+              <a class="td-none footer_content--texts-link font-16 fw-medium text-gray-600"
+                href="./index.php#facilities">Facilities</a>
+            </li>
+            <li class="ls-none">
+              <a class="td-none footer_content--texts-link font-16 fw-medium text-gray-600"
+                href="./index.php#rooms">Rooms</a>
+            </li>
+            <li class="ls-none">
+              <a class="td-none footer_content--texts-link font-16 fw-medium text-gray-600"
+                href="./index.php#events">Events</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-4 footer_content--texts order-2 col-sm-6">
+          <div class="footer_content--texts-header mb-24">
+            <h4 class="font-18 fw-semibold text-black">About Us</h4>
+          </div>
+          <ul class="d-flex flex-column gap-18 p-0">
+            <li class="ls-none">
+              <a class="td-none footer_content--texts-link footer-num font-16 fw-medium text-gray-600"
+                href="tel:9842742225"><i class="fa-solid fa-phone me-12"></i>+977 9842742225</a>
+            </li>
+            <li class="font-16 ls-none fw-medium text-gray-600">
+              <a class="td-none footer_content--texts-link footer-mail font-16 fw-medium text-gray-600"
+                href="mailto:kebab.bahadur@gmail.com"><i class="fa-solid fa-envelope me-12"></i>info@hamrostel.com.np
+              </a>
+            </li>
+            <li class="font-16 ls-none fw-medium text-gray-600">
+              <a target="_blank" class="td-none footer_content--texts-link font-16 fw-medium text-gray-600"
+                href="https://www.google.com/maps/place/Herald+College+Kathmandu/@27.712094,85.3281912,17z/data=!3m1!4b1!4m6!3m5!1s0x39eb196de5da5741:0x652792640c70ede9!8m2!3d27.712094!4d85.3307661!16s%2Fg%2F1pyqqt1lz?entry=ttu"><i
+                  class="fa-solid fa-location-dot me-12"></i>Naxal,
+                Kathmandu, Nepal
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-----=====-----===== Scripts =====-----=====----->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script src="./js/swiper.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+  </script>
+  <script src="https://kit.fontawesome.com/4cbeaa27da.js" crossorigin="anonymous"></script>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+
+  <script src="./js/script.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
